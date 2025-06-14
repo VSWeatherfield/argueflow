7271f5bfdbf67e95716e2b9b662a149df04665fa
Vladimir Smirnov <voff.smirnoff@gmail.com>
2025-06-14 08:40:15 +0300

    Update MLFlow documentation
    Add callback on the best checkpoint

--- argueflow/train/train.py
+++ argueflow/train/train.py
@@ -1,9 +1,12 @@
 import logging
 import os
 
+import mlflow
+from git import Repo
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 from argueflow.data.argue_module import FeedbackPrize2DataModule
 from argueflow.models.fp2_lightning_module import FeedbackPrize2LightningModule
@@ -25,10 +28,22 @@ def train(cfg: DictConfig):
     log.info("Initializing Lightning Module...")
     model = FeedbackPrize2LightningModule(cfg)
 
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_f1",
+        mode="max",
+        save_top_k=1,
+        filename="best_model",
+        save_last=True,
+    )
+
+    repo = Repo(search_parent_directories=True)
+    mlflow.set_tags({"git_commit": repo.head.object.hexsha})
+
     log.info("Starting training with PyTorch Lightning...")
     trainer = Trainer(
         **cfg.trainer,
         logger=instantiate(cfg.logger),
+        callbacks=[checkpoint_callback],
     )
 
     trainer.fit(model, datamodule=dm)
